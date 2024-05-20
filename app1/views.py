f6a17a336b58beb78d9efc211f4f2547259758c2
saki <srinivasulu@rekonsys.tech>
2024-05-20 14:28:19 +0530

    commit

---
+++ app1/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def create(request):
+    return render(request, 'index.html')
+
+
+def profile(request):
+    return render(request, 'profile.html')
